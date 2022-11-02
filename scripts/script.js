c221738c21dbf3526f1179d4338afd0f6b46dac5
johnjc0802 <100451448+johnjc0802@users.noreply.github.com>
2022-11-02 14:21:55 -0700

    Updated for latest version with localStorage and form 11/2/22

--- scripts/script.js
+++ scripts/script.js
@@ -4,95 +4,134 @@ var ingrCount = 0;
 var txtLength = 0;
 /******** For testing localStorage *********************
 /*const recipeStorage = [
-    {'name':'juice','img':'https://www.gs1india.org/media/Juice_pack.jpg','description':'Orange and Apple juice fresh and delicious'},
-    {'name':'Tayto','img':'https://www.irishtimes.com/polopoly_fs/1.4078148!/image/image.jpg','description':'Cheese & Onion Chips'}
+    {'title':'Napolitan Tomato Sauce','imgURL':'images/NapolitanSauce.jpg',
+    'description':'My wife's Family Recipe for Napolitan Style Tomato Sauce!'ingredients':{},'instructions': {}},
+    {'name':'Grilled Cheese','img':'grilled-cheese-sandwich.jpg','description':'Cheese & Onion Chips'}
 ];
 
 localStorage.setItem("recipeStorage", JSON.stringify(cookBook.recipes));
 */
 showNavbarHeader();
 showFooter();
+
 //initInput();
 //initTextarea();
 
-function initInput(){
-  for (i=1; i < document.getElementsByTagName("input").length; i++){
-  document.getElementsByTagName("input")[i].style.background = "transparent";
-  }
-}
-
-function initTextarea(){
-  for (i=0; i < document.getElementsByTagName("textarea").length; i++){
-  document.getElementsByTagName("textarea")[i].style.background = "transparent";
-  }
-}
-
 class RecipeController {
      constructor(currentId=0) {
-         this.recipes = []; 
          this.currentId = currentId;
+         this.recipes = []; 
      };
 
-     addRecipe(title, description, imgURL, createdAt){ 
+     addRecipe(cuisine, title, description, imgURL, createdAt, ingredients, instructions){ 
          const recipe = {   //recipe is an object
-             id: this.currentId++,
+             //id: this.currentId++,
+             id: this.recipes.length,
+             cuisine: cuisine,
              title: title,
              description: description,
-             imgURL: imgURL,
-             createdAt: createdAt
+             imgURL: (imgURL ? imgURL : "images/DefaultImage.jpg"),
+             createdAt: createdAt,
+             ingredients: ingredients,
+             instructions: instructions
          };
 
          this.recipes.push(recipe); 
      };
-
-     /******** For testing localStorage *********************
-     loadRecipesFromLocalStorage() {
-        const storageItems = localStorage.getItem("recipes")
-        if (storageItems) {
-            const recipes = JSON.parse(storageItems)
-            //TODO load the items into the local items structure (this.items)           
-        }
-    }*******************************************************/
 };
 
 cookBook = new RecipeController();
-cookBook.addRecipe("Napolitan Tomato Sauce","My wife's Family Recipe for Napolitan Style Tomato Sauce!","./images/NapolitanSauce.jpg","09/23/2022");
-cookBook.addRecipe("Roman Tomato Sauce","My wife's Family Recipe for Roman Style Tomato Sauce!","./images/RomanSauce.jpg","09/23/2022");
-cookBook.addRecipe("Turino Tomato Sauce","My wife's Family Recipe for Turino Style Tomato Sauce!","./images/san-marzano-tomato-sauce-2.jpg","09/23/2022");
-cookBook.addRecipe("Borscht","Everyone knows what it is and many people around the world have fallen in love with this iconic beet soup.","./images/Borscht.jpg","09/23/2022");
-cookBook.addRecipe("Grilled Cheese","Old Fashioned Grilled Cheese Sandwich - like mom made","./images/grilled-cheese-sandwich.jpg","09/24/2022");
-cookBook.addRecipe("Chunky Blue Cheese Burger - Perfect For Football","A hamburger for the millenium - juicy ground beef and chunks of bleu cheese!","./images/Chunky Blue Cheese Burger.jpg","09/23/2022");
-cookBook.addRecipe("Beef Stroganoff","Golden seared juicy beef strips smothered in a sour cream mushroom gravy","./images/Classic Beef Stroganoff.jpg","09/23/2022");
-cookBook.addRecipe("Ultimate Oven Barbecued Ribs","These easy oven baked ribs with brown sugar are fall-off-the bone delicious.","./images/Grilled-BBQ-Ribs-8.jpg","09/23/2022");
-//console.log(cookBook.addRecipe[2]);
 
-for (i=0; i < cookBook.recipes.length; i++){
-    addRecipeCard(cookBook.recipes[i]);
+if (document.getElementById("index")) {
+// initialize the cookbook with some hard coded values //
+cookBook.addRecipe("Italian","Napolitan Tomato Sauce","My wife's Family Recipe for Napolitan Style Tomato Sauce!",
+  "./images/NapolitanSauce.jpg","09/23/2022",{1:"6 10oz cans of Hunt's tomato sauce", 2: "2 oz  tomato paste"},{1:"Add sauce, tomato paste, water into a 12 qt pot", 2:"Mix over medium heat"});
+cookBook.addRecipe("Italian","Roman Tomato Sauce","My wife's Family Recipe for Roman Style Tomato Sauce!","./images/RomanSauce.jpg","09/23/2022",{},{});
+cookBook.addRecipe("Italian","Turino Tomato Sauce","My wife's Family Recipe for Turino Style Tomato Sauce!","./images/san-marzano-tomato-sauce-2.jpg","09/23/2022",{},{});
+cookBook.addRecipe("Ukrainian","Borscht","Everyone knows what it is and many people around the world have fallen in love with this iconic beet soup.","./images/Borscht.jpg","09/23/2022",{},{});
+cookBook.addRecipe("American","Grilled Cheese","Old Fashioned Grilled Cheese Sandwich - like mom made","./images/grilled-cheese-sandwich.jpg","09/24/2022",{},{});
+cookBook.addRecipe("American","Chunky Blue Cheese Burger - Perfect For Football","A hamburger for the millenium - juicy ground beef and chunks of bleu cheese!","./images/Chunky Blue Cheese Burger.jpg","09/23/2022",{},{});
+cookBook.addRecipe("Russian","Beef Stroganoff","Golden seared juicy beef strips smothered in a sour cream mushroom gravy","./images/Classic Beef Stroganoff.jpg","09/23/2022",{},{});
+cookBook.addRecipe("Tex-Mex","Ultimate Oven Barbecued Ribs","These easy oven baked ribs with brown sugar are fall-off-the bone delicious.","./images/Grilled-BBQ-Ribs-8.jpg","09/23/2022",{},{});
+localStorage.setItem("recipes", JSON.stringify(cookBook.recipes));
 };
 
+function loadRecipesFromLocalStorage() {
+  cookBook.recipes.length = 0;  
+  const storageRecipes = localStorage.getItem("recipes");
+  if (storageRecipes) {
+    const recipes = JSON.parse(storageRecipes);
+    for(let i=0; i < recipes.length; i++){
+      const recipe = recipes[i];
+      cookBook.recipes.push(recipe);
+    }
+  }
+};
 
-function addRecipeCard(recipe) {
+loadRecipesFromLocalStorage();
+
+if (document.getElementById("list")) {
+//Load the recipes into the local items structure (this.recipes)
+//alert("before cards: " + cookBook.recipes.length); 
+  for (i=0; i < cookBook.recipes.length; i++){
+    addRecipeCard(cookBook.recipes[i]);
+  };
+
+  function addRecipeCard(recipe) {
     const cardHTML = '<div class="card border border-dark m-1" style="width: 19rem;">\n' +
       '    <div class="card-body">\n' +
-        '    <p>Cuisine(coming soon)</p><img class="card-img-top" src="' + recipe.imgURL + '" width="400" height="250"  alt="Recipe Image">\n' +
+        '    <p><strong>Cuisine: ' + recipe.cuisine + ' id: ' + recipe.id + '</strong></p><img class="card-img-top" src="' + recipe.imgURL + '" width="400" height="250"  alt="Recipe Image">\n' +
         '        <h5 class="card-title">' + recipe.title + '</h5>\n' +
-        '        <p class="card-text">' + recipe.description + '</p>\n' + 
+        '        <p class="card-text">' + recipe.description + '<br>' + recipe.createdAt + '</p>\n' + 
         '        <div class="btn-group-sm">\n' +
-        '          <a href="recipe.html" class="btn btn-primary btn-sm m-1">View</a>\n' +
+        '          <a href="recipe.html" class="btn btn-primary btn-sm m-1">View</a>\n' + 
         '        </div>\n' +
         '  </div>\n' +
-        '</div>\n' +
+        '</div>\n' + 
         '<br/>';
  
     const cardsContainer = document.getElementById("list-items");
     cardsContainer.innerHTML += cardHTML;
+  };
 };
-
+function saveForm(){
+  //alert("before: " + cookBook.recipes.length);  
+    var newCuisine = document.getElementById("recipeCuisine").value;
+    var newTitle = document.getElementById("recipeTitle").value;
+    var newDescription = document.getElementById("recipeDescription").value;
+    var newImageURL = document.getElementById("recipeImageUrl").value;
+    const newDate = new Date();
+    newCreatedAt = newDate.toLocaleDateString();
+    cookBook.addRecipe(newCuisine,newTitle,newDescription,newImageURL,newCreatedAt,{},{});
+    localStorage.setItem("recipes", JSON.stringify(cookBook.recipes));
+  //alert("after: " + cookBook.recipes.length + " " + cookBook.recipes[(cookBook.recipes.length - 1)].title);
+  // Recipe input form fields display changed on save
+    const inputElements = document.getElementsByTagName("input");
+    document.getElementById("recipeTitle").style.textDecoration = "underline";
+    document.getElementById("recipeTitle").style.fontWeight = "600";
+    //alert(inputElements.length);
+    for (i=0; i < inputElements.length; i++){
+      inputElements[i].style.background = "transparent";
+      inputElements[i].style.border = "none";
+      if (inputElements[i].value == ""){
+        inputElements[i].placeholder = "";
+      }
+    }
+    const textareaElements = document.getElementsByTagName("textarea");
+    for (i=0; i < textareaElements.length; i++){
+      textareaElements[i].style.background = "transparent";
+      textareaElements[i].style.border = "none";
+      if (textareaElements[i].value == ""){
+        textareaElements[i].placeholder = "";
+      }
+    }
+  }
+  
 function showNavbarHeader () {
     document.getElementById("navbar-header").innerHTML = 
     '  <!--navbar component-->' +
     '  <nav class="navbar navbar-expand-lg navbar-light bg-light">' +  
-    '    <a class="navbar-brand" href="https://johnjc0802.github.io">' +  
+    '    <a class="navbar-brand" href="https://olenarub.github.io/tastebuds/">' +  
     '      <img src="images/tastebuds.jpg" alt="Tastebuds Logo" height="70"/>' +  
     '    </a>' +  
     '    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">' +  
@@ -104,7 +143,7 @@ function showNavbarHeader () {
     '        <a class="nav-link" href="./index.html">Home</a>' +  
     '      </li>' +  
     '      <li class="nav-item">' +  
-    '        <a class="nav-link" href="about-project.html">About Us</a>' +  
+    '        <a class="nav-link" href="about.html">About Us</a>' +  
     '      </li>' +  
     '      <li class="nav-item">' +  
     '        <a class="nav-link" href="list.html">Search Recipes</a>' +  
@@ -187,6 +226,18 @@ function addIngredient () {
       '<li><input type="text" placeholder="ingedient_' + ingrSeqNbr + '" class="form-control" id="ingredient_' + ingrSeqNbr + '"></li>';
 };
 
+function initInput(){
+  for (i=1; i < document.getElementsByTagName("input").length; i++){
+  document.getElementsByTagName("input")[i].style.background = "transparent";
+  }
+}
+
+function initTextarea(){
+  for (i=0; i < document.getElementsByTagName("textarea").length; i++){
+  document.getElementsByTagName("textarea")[i].style.background = "transparent";
+  }
+}
+
 function hideBG(elemId){
   //alert(document.getElementById(elemId).id);
   document.getElementById(elemId).style.background = "transparent";
@@ -228,17 +279,26 @@ function nextInst(instElem){
 function chkAddRow(inElem, event){
   txtLength++;
   var eKey = event.key;
-  //alert(eKey + " " + txtLength);
   if (eKey == "Enter" || txtLength > 95) {
     inElem.rows = inElem.rows + 1;
     txtLength = 0;
   }
 }
 function redBg(elem){
-  //alert("color");
   elem.style.background = "red";
 }
-function saveForm(){
+/*function saveForm(){
+//alert("before: " + cookBook.recipes.length);  
+  var newCuisine = document.getElementById("recipeCuisine").value;
+  var newTitle = document.getElementById("recipeTitle").value;
+  var newDescription = document.getElementById("recipeDescription").value;
+  var newImageURL = document.getElementById("recipeImageUrl").value;
+  const newDate = new Date();
+  newCreatedAt = newDate.toLocaleDateString();
+  cookBook.addRecipe(newCuisine,newTitle,newDescription,newImageURL,newCreatedAt,{},{});
+  localStorage.setItem("recipes", JSON.stringify(cookBook.recipes));
+//alert("after: " + cookBook.recipes.length + " " + cookBook.recipes[(cookBook.recipes.length - 1)].title);
+// Recipe input form fields display changed on save
   const inputElements = document.getElementsByTagName("input");
   document.getElementById("recipeTitle").style.textDecoration = "underline";
   document.getElementById("recipeTitle").style.fontWeight = "600";
@@ -258,7 +318,8 @@ function saveForm(){
       textareaElements[i].placeholder = "";
     }
   }
-}
+}*/
+
 
 /*function swapTxt(){
   ingrCount = ingrCount + 1;
